d0b8090ea3bd29a3b54af6d631410fd9eb54ba5b
Grace Wu <gracewu@GracedeMacBook-Air.local>
2026-07-25 14:08:46 +0800

    Deploy static site from portal-fe 937c0ad

---
+++ _next/static/chunks/1dx5z0xa5d09b.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,95546,e=>{"use strict";var t=e.i(70588),i=e.i(37385);e.s(["LocalizedText",0,function({value:e}){let{lang:n}=(0,i.useI18n)();return(0,t.jsx)(t.Fragment,{children:e[n]??e.en})}])}]);
